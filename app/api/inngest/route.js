f60709f96cf7a36e20941ff189c8c8b3f6348aea
JuvitaSaini09 <juvitasaini@gmail.com>
2025-06-02 01:01:26 +0530

    added the cron job for sending email

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,7 +1,8 @@
 import { inngest } from "@/lib/inngest/client";
+import { paymentReminders } from "@/lib/inngest/payment-reminders";
 import { serve } from "inngest/next";
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [],
+  functions: [paymentReminders],
 });
